de2f5c274e8a77882e46ac688b100b686028b8bb
Aditya Kumar <adityakumar2019.ak@gmail.com>
2026-04-03 22:02:50 +0530

    [LAMATIC-COMMIT] Updated flow

--- lamatic/flows/RAGChatbot.ts
+++ lamatic/flows/RAGChatbot.ts
@@ -4,16 +4,159 @@ const flowConfig = {
   "nodes": [
     {
       "id": "triggerNode_1",
-      "data": {},
+      "data": {
+        "modes": {},
+        "nodeId": "chatTriggerNode",
+        "values": {
+          "chat": "",
+          "domains": [
+            "*"
+          ],
+          "nodeName": "Chat Widget",
+          "chatConfig": {
+            "botName": "Lamatic Bot",
+            "imageUrl": "https://img.freepik.com/premium-vector/robot-android-super-hero_111928-7.jpg?w=826",
+            "position": "right",
+            "policyUrl": "https://lamatic.ai/docs/legal/privacy-policy",
+            "displayMode": "popup",
+            "placeholder": "Compose your message",
+            "suggestions": [
+              "What is lamatic?",
+              "How do I add data to my chatbot?",
+              "Explain this product to me"
+            ],
+            "errorMessage": "Oops! Something went wrong. Please try again.",
+            "hideBranding": false,
+            "primaryColor": "#ef4444",
+            "headerBgColor": "#000000",
+            "greetingMessage": "Hi, I am Lamatic Bot. Ask me anything about Lamatic",
+            "headerTextColor": "#FFFFFF",
+            "showEmojiButton": true,
+            "suggestionBgColor": "#f1f5f9",
+            "userMessageBgColor": "#FEF2F2",
+            "agentMessageBgColor": "#f1f5f9",
+            "suggestionTextColor": "#334155",
+            "userMessageTextColor": "#d12323",
+            "agentMessageTextColor": "#334155"
+          }
+        },
+        "trigger": true
+      },
       "type": "triggerNode",
+      "measured": {
+        "width": 216,
+        "height": 93
+      },
       "position": {
         "x": 0,
         "y": 0
-      }
+      },
+      "selected": false
+    },
+    {
+      "id": "LLMNode_544",
+      "data": {
+        "label": "dynamicNode node",
+        "modes": {},
+        "nodeId": "LLMNode",
+        "values": {
+          "id": "LLMNode_544",
+          "tools": [],
+          "prompts": [
+            {
+              "id": "187c2f4b-c23d-4545-abef-73dc897d6b7b",
+              "role": "system",
+              "content": "use simple language to write the prompt\n{{triggerNode_1.output.chatMessage}}"
+            },
+            {
+              "id": "187c2f4b-c23d-4545-abef-73dc897d6b7d",
+              "role": "user",
+              "content": "Write your prompt here"
+            }
+          ],
+          "memories": "[]",
+          "messages": "[]",
+          "nodeName": "Generate Text",
+          "attachments": "",
+          "credentials": "",
+          "generativeModelName": [
+            {
+              "type": "generator/text",
+              "params": {},
+              "configName": "configA",
+              "model_name": "gemini/gemini-2.5-flash",
+              "credentialId": "c97fde93-9047-4eec-ad96-6be1476e8aab",
+              "provider_name": "gemini",
+              "credential_name": "lamatic-testing"
+            }
+          ]
+        }
+      },
+      "type": "dynamicNode",
+      "measured": {
+        "width": 216,
+        "height": 93
+      },
+      "position": {
+        "x": 0,
+        "y": 130
+      },
+      "selected": false
+    },
+    {
+      "id": "responseNode_triggerNode_1",
+      "data": {
+        "label": "Response",
+        "nodeId": "chatResponseNode",
+        "values": {
+          "id": "responseNode_triggerNode_1",
+          "content": "{{LLMNode_544.output.generatedResponse}}",
+          "nodeName": "Chat Response",
+          "references": "{{triggerNode_1.output.chatMessage}}",
+          "webhookUrl": "{{LLMNode_544.output.generatedResponse}}",
+          "webhookHeaders": ""
+        },
+        "isResponseNode": true
+      },
+      "type": "responseNode",
+      "measured": {
+        "width": 216,
+        "height": 93
+      },
+      "position": {
+        "x": 0,
+        "y": 260
+      },
+      "selected": true
     }
   ],
-  "edges": [],
-  "status": "active",
+  "edges": [
+    {
+      "id": "triggerNode_1-LLMNode_544",
+      "type": "defaultEdge",
+      "source": "triggerNode_1",
+      "target": "LLMNode_544",
+      "sourceHandle": "bottom",
+      "targetHandle": "top"
+    },
+    {
+      "id": "LLMNode_544-responseNode_triggerNode_1",
+      "type": "defaultEdge",
+      "source": "LLMNode_544",
+      "target": "responseNode_triggerNode_1",
+      "sourceHandle": "bottom",
+      "targetHandle": "top"
+    },
+    {
+      "id": "response-trigger_triggerNode_1",
+      "type": "responseEdge",
+      "source": "triggerNode_1",
+      "target": "responseNode_triggerNode_1",
+      "sourceHandle": "to-response",
+      "targetHandle": "from-trigger"
+    }
+  ],
+  "status": "inactive",
   "created_at": "2026-04-03T12:23:41.957402+00:00"
 };
 
